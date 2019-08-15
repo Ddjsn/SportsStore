9b9e2e976155ab6d63e01e5a1c5952805195f166
wyl <1315210522@qq.com>
2019-08-15 22:55:33 +0800

    修改

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Mvc;
+﻿using Moq;
 using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -28,7 +30,15 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product{Name="ab",Price=10 },
+                new Product{Name="cd",Price=132 },
+                new Product{Name="asdfi",Price =13 },
+            });
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+
         }
     }
 }
